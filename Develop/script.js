5d9d96c78d4d57afd01108422805748affcfe2e8
John Davis <johnedavis624@gmail.com>
2020-01-26 20:10:29 -0600

    loop

--- Develop/script.js
+++ Develop/script.js
@@ -19,7 +19,33 @@ $(document).ready(function() {
     let currentDay = $('#currentDay');
     currentDay.text(now);
     
-
+    // change description here - none
+    const saveIcon = ("./images/save-regular.svg"); 
+  
+    // Get stored todos from localStorage
+    // Parsing the JSON string to an object
+    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+  
+    if (test) { console.log(storedPlans); }
+  
+    // If plans were retrieved from localStorage, update the plan array to it
+    if (storedPlans !== null) {
+      planTextArr = storedPlans;
+    } else {
+      // this should only occur on first time the app is loaded in the browser
+      // helpfully remind user that lunch is important
+      planTextArr = new Array(9);
+      planTextArr[4] = "Picnic lunch outside";
+    }
+  
+    if (test) { console.log("full array of plned text",planTextArr); }
+  
+    // set variable referencing planner element
+    let plannerDiv = $('#plannerContainer');
+    // clear existing elements
+    plannerDiv.empty();
+  
+    if (test) { console.log("current time",nowHour12); }
   });
 
 
